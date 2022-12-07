a47721f1004f814ae61eafd2dc85237d9be831e5
BOUCHENNA RAFIK p2200950 <p2200950@univ-lyon1.fr>
2022-12-07 17:08:29 +0100

    fonction.h

--- fonction.h
+++ fonction.h
@@ -3,27 +3,29 @@
 #define VILLE 100
 #define TEL 15
 struct Client_{
-        char NomAnnuaire[SIZE];
-        char Nom[SIZE];
-        char prenom[SIZE];
-        char codePostal[SIZE];
-        char Ville[VILLE];
-        char NumTel[TEL];
-        char Adresse_mail[SIZE];
-        char profession[SIZE];
-        //Variable pour trie des clients
-        char RecNom[SIZE];
-//char NomRecherche[SIZE];
 
-/*char rechercheNom[size];*/
+        char nom_p[SIZE];
+        char prenom_p[SIZE];
+        char code_postal_p[SIZE];
+        char ville_p[VILLE];
+        char telephone_p[TEL];
+        char mel_p[SIZE];
+        char profession_p[SIZE];
+        //Variable pour trie des clients
+        char RecAdresse[SIZE];
+//char nom_pRecherche[SIZE];
+
+/*char recherchenom_p[size];*/
 
 };
 typedef struct Client_ Client;
-void ajoutClient(Client *Id_client);
+void ajoutClient( const Client *Id_client);
 void AffichageAnnuaire(const Client *Id_client);
+void verifeClient( Client *Id_client);
 void  RechercheClient(const Client *Id_client);
-void tri_client( Client *Id_client);
-void saisirClient(  Client *Id_client);
+//void tri_client( Client *Id_client, Id_client->nom_p);
+void saisirClient(   Client *Id_client);
+void CopieAnnuaireFichier(const Client *Id_client);
 
 
 //int CompareChaine(const  void  *a,const  void  *b);
