6211dc2fa959927d82fcfbcb37a2fd943f58f387
BOUCHENNA RAFIK p2200950 <rafik.bouchenna@etu.univ-lyon1.fr>
2022-12-18 21:02:26 +0000

    solution trouver pour determiner la validité de l'annuaire ,pas de teste de la solution sur l'annuaire mais sur un tableau de chaines de caracteres ,reste a entrer les valeurs  du fichier contenant les mails dans le tableau Valeurs

--- main.c
+++ main.c
@@ -7,29 +7,81 @@
 
 /*void(const afficher  personne)*pp*/
 
+/*
+struct A {
 
+
+char * ad;
+char ch[50];
+
+}
+typedef struct A A;
+void saisie(A*a){
+
+a->ad =   (char *) malloc (20*sizeof(char));
+fgets(a->ad,20,stdin);
+}
+double *p = (double*)malloc(10*sizeof(double));
+char *c = (char*)malloc(15*sizeof(char));
+
+*/
+void doublons(char *doub[], int taille)
+ {
+
+   int i, j;
+
+   for (i = 0; i < taille; i++)
+    for (j = i+1; j < taille; j++)
+      if (stricmp(doub[i], doub[j]) == 0)
+        {
+          printf("\nil ya des doublons\n");
+        printf("\nles doublons sont : %s et %s",doub[i], doub[j]);
+
+        }
+
+  }
 
 int main()
 {
+//double *p = (double*)malloc(10*sizeof(double));//80 octets
+//char *c = (char*)malloc(15*sizeof(char));//15 octets
+//calloc : initialise tout a 0 voir -> cppreference
 
 
     Client r;
 
-int n = 0;
+
 //int m  =sizeof((&r)->nom)/256;
 
     printf("/*******************Annuaire Client*******************\n");
     //Preparation du fichier texte et affichage de l'annuaire
-AffichageAnnuaire(&r);
-CopieAnnuaireFichier(&r);
-verifeClient(&r);
-//Ajout du client
+//verifeClient(&r);
+//ajoutClient(&r);
+
+//saisirClient(&r);
 /*
-saisirClient(&r);
 verifeClient(&r);
 ajoutClient(&r);
 */
-//
+
+
+
+   char *valeurs[] = {"rafik", "Aziz", "Ras", "walid", "walid"};
+
+
+   doublons(valeurs, 5);
+
+ return 0;
 }
+
+
+
+
+
+
+
+
+
+
 
 
