37fcec7d2e552064c5e258af3962e2fdb07fcf3c
BOUCHENNA RAFIK p2200950 <rafik.bouchenna@etu.univ-lyon1.fr>
2022-12-19 21:51:47 +0000

    création  de la fonction verife_annuaire

--- fonction.h
+++ fonction.h
@@ -1,37 +1,50 @@
+/**< \file fonction.h */
 #pragma once
 #define SIZE 100
 #define VILLE 50
 #define TEL 15
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+//attention au warning
+//declaration de la structure client
 struct Client_{
 
-        char nom_p[SIZE];
-        char prenom_p[SIZE];
-        char code_postal_p[SIZE];
-        char ville_p[VILLE];
-        char telephone_p[TEL];
-        char mel_p[SIZE];
-        char profession_p[SIZE];
-        //Variable pour trie des clients
-        //char RecAdresse[SIZE];
-//char nom_pRecherche[SIZE];
-
-/*char recherchenom_p[size];*/
+        char nom_p[SIZE];//taille a changer avec l'allocation dynamique...
+        char prenom_p[SIZE];//taille a changer avec l'allocation dynamique...
+        char code_postal_p[SIZE];//taille a changer avec l'allocation dynamique...
+        char ville_p[VILLE];//taille a changer avec l'allocation dynamique...
+        char telephone_p[TEL];//taille a changer avec l'allocation dynamique...
+        char mel_p[SIZE];//taille a changer avec l'allocation dynamique...
+        char profession_p[SIZE];//taille a changer avec l'allocation dynamique...
 
 };
-typedef struct Client_ Client;//attention au warning
+typedef struct Client_ Client;//notation moins lourde
 void ajoutClient(const  Client *Id_client);
 void AffichageAnnuaire(const Client *Id_client);
 void verifeClient( Client *Id_client);
 void  RechercheClient(const Client *Id_client);
-//void tri_client( Client *Id_client, Id_client->nom_p);
 void saisirClient(   Client *Id_client);
 void CopieAnnuaireFichier(const Client *Id_client);
 
 void verif_annuaire(const Client *Id_client);
 //int CompareChaine(const  void  *a,const  void  *b);
 //return strcmp(pa,pb);
+//déclaration de la structure doublons pour tester la validité de l'annuaire
+struct doublons_{
+char temp[1000];//taille a changer avec l'allocation dynamique...
+char donnes[1000][1000];//taille a changer avec l'allocation dynamique...
+int ligne;
 
 
+ };
+
+ typedef struct doublons_ doublons;//notation moins lourde
+void tri_client(const doublons *d);
 
 
 
