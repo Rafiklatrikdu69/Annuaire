3ffa357ef85b03b6d5a419768fd7d30aad37fc64
BOUCHENNA RAFIK p2200950 <rafik.bouchenna@etu.univ-lyon1.fr>
2022-12-20 18:02:43 +0000

    mise en place des fonctions sans l'allocation  dynamique ,reste l'allocation dynamique ! A tester

--- fonction.c
+++ fonction.c
@@ -3,8 +3,9 @@
 #include <stdlib.h>
 #include<string.h>
 #include "fonction.h"
-Client s1;
-void AffichageAnnuaire(const Client *Id_client){
+
+
+void Afficher_annuaire_clients(const Client *Id_client){
  FILE*fichier;
    fichier = fopen("fichier.txt","r");//ouverture du fichier en mode lecture
    char c;
@@ -38,17 +39,9 @@ void AffichageAnnuaire(const Client *Id_client){
    fclose(fichier);//ferme le fichier
 
 
-
-
-
-
-
-
-
-
 }
 
-void CopieAnnuaireFichier(const Client *Id_client){
+void Ecriture_annuaire_clients(const Client *Id_client){
 
     FILE*fichier;
     FILE*fichier2;
@@ -69,65 +62,7 @@ void CopieAnnuaireFichier(const Client *Id_client){
 
 
 
-
-void saisirClient(  Client * Id_client){
-//cette fonction permet a l'utilisateur d'entrer ces donnes -> utilsation de fgets pour les chaines de  caracteres
-
-;
-            printf("La premiere lettre doit etre en majuscule ! \n");
-
-
-            printf("Nom\n");
-            fgets(Id_client->nom_p,sizeof(Id_client->nom_p),stdin);
-            Id_client->nom_p[strcspn(Id_client->nom_p,"\r\n")] = '\0';
-
-
-
-
-            printf("Prenom\n");
-            fgets(Id_client->prenom_p,sizeof(Id_client->prenom_p),stdin);
-            Id_client->prenom_p[strcspn(Id_client->prenom_p,"\r\n")] = '\0';
-
-
-
-
-            printf("Code postal\n");
-            fgets(Id_client->code_postal_p,sizeof(Id_client->code_postal_p),stdin);
-            Id_client->code_postal_p[strcspn(Id_client->code_postal_p,"\r\n")] = '\0';
-
-
-
-
-            printf("Ville\n");
-            fgets(Id_client->ville_p,sizeof(Id_client->ville_p),stdin);
-            Id_client->ville_p[strcspn(Id_client->ville_p,"\r\n")] = '\0';
-
-
-            printf("Numero de telephone\n");
-            fgets(Id_client->telephone_p,sizeof(Id_client->telephone_p),stdin);
-            Id_client->telephone_p[strcspn(Id_client->telephone_p,"\r\n")] = '\0';//si la chaine depasse la taille alors le dernier caractere sera un \0
-
-
-
-
-
-            ajoutClient(&s1);//appel la fonction qui va permetre a l'utilisateur d'entrer son adresse mail en verifiant que l'adresse n'est pas deja prise
-
-
-
-
-
-            printf("profession_p\n");
-            fgets(Id_client->profession_p,sizeof(Id_client->profession_p),stdin);
-            Id_client->profession_p[strcspn(Id_client->profession_p,"\r\n")] = '\0';
-
-
-
-
-
-}
-
-void verifeClient( Client *Id_client){
+void Separation_mel_p( Client *Id_client){
 //cette fonction permet de séparer les emails dans un fichier a part pour par la suite verifier si la chaine mel_p est présente
 FILE*fichier;
 FILE*fichier2;
@@ -176,47 +111,88 @@ FILE*fichier2;
 
 
 
-void ajoutClient(  const Client *Id_client){
+void ajouter_Client(  const Client *Id_client){
 //cette fonction compare mel_p donner en entrée et verifier si elle est deja présente dans le fichier
+//cette fonction permet a l'utilisateur d'entrer ces donnes -> utilsation de fgets pour les chaines de  caracteres
+    Client p;
+
 FILE*fichier;
 fichier = fopen("fichier2.txt","r");//ouverture du fichier en lecture pour lire les adresses mails
 char chaine[100];//taille a changer avec l'allocation dynamique...
 char temp[256];//cette variable permet de stocker temporairement la chaine -> taille a changer avec l'allocation dynamique...
 char chaine2[256];//taille a changer avec l'allocation dynamique...
-    printf("Adresse mail\n");
-    fgets(chaine,sizeof(chaine),stdin);
-            chaine[strcspn(chaine,"\n")] = '\0';//la fonction strcspn permet de gerer le cas ou chaine est egale au nombre de caracteres en prenant en compte \n
-    while(!feof(fichier)){
+   printf("La premiere lettre doit etre en majuscule ! \n");
+
+
+            printf("Nom\n");
+            fgets(p.nom_p,sizeof(p.nom_p),stdin);//fgets->saisie de chaine de caracteres
+            p.nom_p[strcspn(p.nom_p,"\r\n")] = '\0';
+
+
+
+
+            printf("Prenom\n");
+            fgets(p.prenom_p,sizeof(p.prenom_p),stdin);
+            p.prenom_p[strcspn(p.prenom_p,"\r\n")] = '\0';
+
+
+
+
+            printf("Code postal\n");
+            fgets(p.code_postal_p,sizeof(p.code_postal_p),stdin);
+            p.code_postal_p[strcspn(p.code_postal_p,"\r\n")] = '\0';
+
+
+
+
+            printf("Ville\n");
+            fgets(p.ville_p,sizeof(p.ville_p),stdin);
+            p.ville_p[strcspn(p.ville_p,"\r\n")] = '\0';
+
+
+            printf("Numero de telephone\n");
+            fgets(p.telephone_p,sizeof(p.telephone_p),stdin);
+            p.telephone_p[strcspn(p.telephone_p,"\r\n")] = '\0';//si la chaine depasse la taille alors le dernier caractere sera un \0
+
+            printf("Adresse mail\n");
+            fgets( p.mel_p,sizeof(p.mel_p),stdin);
+            p.mel_p[strcspn(p.mel_p,"\n")] = '\0';//la fonction strcspn permet de gerer le cas ou chaine est egale au nombre de caracteres en prenant en compte \n
+
+
+            while(!feof(fichier)){
 
                     fscanf(fichier,"%s",chaine2);//cela permet de scanner le fichier
                 //printf("%s",chaine2);
                 strcpy(temp,chaine2);//je copie cette chaine qui est scanner dans ma variable temp
                 //printf("%s\n",temp);
-                    if(strcmp(temp,chaine) == 0){//utilisation strcmp qui permet de comparer deux chaine si les deux chaine sont égales alors la chaine et deja presente donc mel_p n'est pas valide
-                        printf("la chaine :%s existe deja\n",chaine);
-                        break;//pas prztique mais utile pour le deboggage...
+                    if(strcmp(temp, p.mel_p) == 0){//utilisation strcmp qui permet de comparer deux chaine si les deux chaine sont égales alors la chaine et deja presente donc mel_p n'est pas valide
+                        printf("la chaine :%s existe deja\n",p.mel_p);
+                        break;//pas pratique mais utile pour le deboggage...
                     }
                     else{
-
-
                         //printf("la chaine :%s n' existe pas\n",chaine);
                     }
-    }
+            }
 
 
-    fclose(fichier);//je ferme le fichier avec fclose
+            fclose(fichier);//je ferme le fichier avec fclose
+
+
+            printf("profession_p\n");
+            fgets(p.profession_p,sizeof(p.profession_p),stdin);
+            p.profession_p[strcspn(p.profession_p,"\r\n")] = '\0';
+
+
+
+
+
+
+
+
+
+
 }
-void verif_annuaire(const Client *Id_client){
-
-//fonction a venir
-
-
-
-}
-
-
-
-void tri_client(const doublons *d){
+void Verifier_validite_annuaire(const doublons *d){
 FILE*fichier;
 fichier = fopen("fichier2.txt","r");//j'ouvre le fichier en mode lecture pour lire le fichier-> r =read
  int i, j;
@@ -230,8 +206,8 @@ while(!feof(fichier)&& !ferror(fichier)){//si ferror est vrai alors le fichier n
             }
 
 }
-fclose(fichier);
-//utilisation du tri bulle en modifiant quelques parametre
+
+//utilisation du tri bulle en modifiant quelques parametre,cette fois ci je compare si les chaines sont identiques
   for (i = 0; i < r.ligne; i++){
             for (j = i+1; j < r.ligne; j++){
                       if (stricmp(r.donnes[i], r.donnes[j])== 0)//si les chaines sont identiques alors il y'a des doublons
@@ -242,12 +218,26 @@ fclose(fichier);
                         }
             }
    }
-//trie a bulle -> fonctionnement : si l'élement a l'indice i = 0 est supérieur a l'indice i+1 (l'element suivant) alors on permute les elements jusqu'a le tableau soit trié
+   fclose(fichier);
+
+}
+
+
+
+void tri_client(const doublons *d){
+    //fonction de trie a completer en faisant attention aux clients qui n'ont pas renseigné les leur noms
+FILE*fichier;
+fichier = fopen("fichier2.txt","r");//j'ouvre le fichier en mode lecture pour lire le fichier-> r =read
+ int i, j;
+
+doublons r;
+r.ligne = 0;//initialisation du nombre de ligne a 0 a l'aide de la structure doublons -> voir fichier "fonction.h"
+//trie a bulle -> fonctionnement : si l'élement a l'indice i = 0 est supérieur a l'indice i+1 (l'element voisin) alors on permute les elements jusqu'a le tableau soit trié
    for (i = 0; i < r.ligne; i++){
             for (j = i+1; j < r.ligne; j++){
                       if (stricmp(r.donnes[i], r.donnes[j])> 0)//la fonction stricmp permet de comparer des chaines de caracteres en faisant abstraction des majuscules et miniscules
                         {
-                            //permutation des valeurs
+                            //permutation des valeurs avec strcpy-> librairie->string.h
                           strcpy(r.temp,r.donnes[i]);//temp=chaine[i]
                           strcpy(r.donnes[i],r.donnes[j]);//chaine[i]=chaine[j]
                           strcpy(r.donnes[j],r.temp);//chaine[j]=temp
