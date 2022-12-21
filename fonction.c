3d8f375e61da3a4162f8f9a2eb25aaa102f0a6e9
BOUCHENNA RAFIK p2200950 <rafik.bouchenna@etu.univ-lyon1.fr>
2022-12-21 17:58:06 +0000

    teste de l'allocation dynamique->plutot correct

--- fonction.c
+++ fonction.c
@@ -4,7 +4,7 @@
 #include<string.h>
 #include<assert.h>
 #include "fonction.h"
-
+#define TAILLE 100
 
 void Afficher_annuaire_clients(const Client *Id_client){
  FILE*fichier;
@@ -116,20 +116,32 @@ void ajouter_Client(  const Client *Id_client){
 //cette fonction compare mel_p donner en entrée et verifier si elle est deja présente dans le fichier
 //cette fonction permet a l'utilisateur d'entrer ces donnes -> utilsation de fgets pour les chaines de  caracteres
     Client p;
-
+    occurence c;
 FILE*fichier;
 fichier = fopen("fichier2.txt","r");//ouverture du fichier en lecture pour lire les adresses mails
 //char chaine[100];//taille a changer avec l'allocation dynamique...
-char temp[256];//cette variable permet de stocker temporairement la chaine -> taille a changer avec l'allocation dynamique...
-char chaine2[256];//taille a changer avec l'allocation dynamique...
+char *temp = (char *) malloc (50*sizeof(char));//j'alloue une taille de 50 octets etant donnes qu'un char = 1 octet
+char *chaine2 = (char *) malloc (50*sizeof(char));
+p.nom_p = (char *) malloc (100*sizeof(char));
+p.prenom_p = (char *) malloc (100*sizeof(char));
+p.ville_p = (char *) malloc (100*sizeof(char));
+p.mel_p = (char *) malloc (100*sizeof(char));
+p.profession_p = (char *) malloc (100*sizeof(char));
 //p.nom_p =   (char *) malloc (1*sizeof(char));
 //char caracteres_incompatible[100] =";,%'`~&:}{^¤$<>@àéè[]_-+/*¨#!?²123456789";
    printf("La premiere lettre doit etre en majuscule ! \n");
 
 NOM:{
             printf("Nom\n");
-            fgets(p.nom_p,sizeof(p.nom_p),stdin);//fgets->saisie de chaine de caracteres
+            fgets(p.nom_p,TAILLE,stdin);//fgets->saisie de chaine de caracteres
             p.nom_p[strcspn(p.nom_p,"\r\n")] = '\0';
+            if(p.nom_p[0] != '\0'){
+                p.nom_p = (char*)realloc(p.nom_p,strlen(p.nom_p)*sizeof(char));
+            }
+            else{
+                goto PRENOM;
+            }
+
             //apres que l'utilisateur entre ses coordonees je detecte avec une boucle si chaque caracteres et compris entre a et z ou A et Z en se basant sur le code ascii
              // je detecte les erreurs possibles lors de la saisie du numero de telephone ou du code postale
             for(int i = 0;i<strlen(p.nom_p);i++){
@@ -153,8 +165,14 @@ NOM:{
 //puts("les caracteres speciaux ne sont pas acceptees -> prenom !!!\n");
 PRENOM:{
             printf("Prenom\n");
-            fgets(p.prenom_p,sizeof(p.prenom_p),stdin);
+            fgets(p.prenom_p,TAILLE,stdin);
             p.prenom_p[strcspn(p.prenom_p,"\r\n")] = '\0';
+             if(p.prenom_p[0] != '\0'){
+                 p.prenom_p = (char*)realloc(p.prenom_p,strlen(p.prenom_p)*sizeof(char));
+            }
+            else{
+                goto CODEPOSTAL;
+            }
 
             for(int i = 0;i<strlen(p.prenom_p);i++){
                 if((p.prenom_p[i]>='a' && p.prenom_p[i]<='z') || (p.prenom_p[i]>='A' && p.prenom_p[i]<='Z') || p.prenom_p[i]=='-'){
@@ -173,7 +191,8 @@ CODEPOSTAL:{
             printf("Code postal\n");
             fgets(p.code_postal_p,sizeof(p.code_postal_p),stdin);
             p.code_postal_p[strcspn(p.code_postal_p,"\r\n")] = '\0';
-            assert(strlen( p.code_postal_p) == 5);
+if(p.code_postal_p[0]!='\0'){assert(strlen(p.code_postal_p) == 5);}
+
             for(int i = 0;i<strlen(p.code_postal_p);i++){
                 if((p.code_postal_p[i]>='0' && p.code_postal_p[i]<='9')){
                         continue;
@@ -189,8 +208,14 @@ CODEPOSTAL:{
 
 VIL:{
             printf("Ville\n");
-            fgets(p.ville_p,sizeof(p.ville_p),stdin);
+            fgets(p.ville_p,TAILLE,stdin);
             p.ville_p[strcspn(p.ville_p,"\r\n")] = '\0';
+               if(p.ville_p[0] != '\0'){
+                   p.ville_p = (char*)realloc(p.ville_p,strlen(p.ville_p)*sizeof(char));
+            }
+            else{
+                goto TELEPHONE;
+            }
 
 
             for(int i = 0;i<strlen(p.ville_p);i++){
@@ -211,6 +236,7 @@ TELEPHONE:{
             fgets(p.telephone_p,sizeof(p.telephone_p),stdin);
             p.telephone_p[strcspn(p.telephone_p,"\r\n")] = '\0';//si la chaine depasse la taille alors le dernier caractere sera un \0
 
+
             for(int i = 0;i<strlen(p.telephone_p);i++){
                 if((p.telephone_p[i]>='0' && p.telephone_p[i]<='9')){
                         continue;
@@ -226,9 +252,17 @@ TELEPHONE:{
 //puts("les caracteres speciaux ne sont pas acceptees-> adresse mail !!!\n");
 MEL:{
             printf("Adresse mail\n");
-            fgets( p.mel_p,sizeof(p.mel_p),stdin);
+            fgets( p.mel_p,TAILLE,stdin);
             p.mel_p[strcspn(p.mel_p,"\n")] = '\0';//la fonction strcspn permet de gerer le cas ou chaine est egale au nombre de caracteres en prenant en compte \n
-            if(p.mel_p[0]=='\0'){printf("l'adresse mail donne obligatoirement etre renseigner\n");goto MEL;}
+
+                   if(p.mel_p[0] != '\0'){
+                   p.mel_p = (char*)realloc(p.mel_p,strlen(p.mel_p)*sizeof(char));
+            }
+            else{
+                printf("l'adresse mail donne obligatoirement etre renseigner\n");
+                goto MEL;
+            }
+
             for(int i = 0;i<strlen(p.mel_p);i++){
                     /*
                     Cette fonction prend deux chaînes chaine 1  et chaine 2 comme argument ,
@@ -248,18 +282,20 @@ MEL:{
                 }
 
             }
-            int occurrence1 = 0;
-            int occurrence2 = 0;
+
+
+                c.occurrence1 = 0;
+                c.occurrence2 = 0;
                         for(int i  = 0; p.mel_p[i] != '\0'; i++)
                         {
                                if(p.mel_p[i] == '@'){
-                                   occurrence1++;
+                                   c.occurrence1++;
                                }
                                if(p.mel_p[i] == '.'){
-                                   occurrence2++;
+                                   c.occurrence2++;
                                }
                         }
-                        switch(occurrence1){
+                        switch(c.occurrence1){
                         case 1:
                             break;
                         default:
@@ -267,7 +303,7 @@ MEL:{
                             goto MEL;
 
                         }
-                         switch(occurrence2){
+                         switch(c.occurrence2){
                         case 1:
                             break;
                         case 2:
@@ -278,14 +314,18 @@ MEL:{
 
                         }
 
-                         printf("Le nombre d’occurrence de @ est %d\n", occurrence1);
-                         printf("Le nombre d’occurrence de . est %d\n", occurrence2);
+                         printf("Le nombre d’occurrence de @ est %d\n", c.occurrence1);
+                         printf("Le nombre d’occurrence de . est %d\n", c.occurrence2);
 
             while(!feof(fichier)){
 
                     fscanf(fichier,"%s",chaine2);//cela permet de scanner le fichier
                 //printf("%s",chaine2);
+                chaine2 = (char*)realloc(chaine2,strlen(chaine2)*sizeof(char));
+                temp = (char*)realloc(chaine2,strlen(chaine2)*sizeof(char));
+                //printf("%d",strlen(temp));
                 strcpy(temp,chaine2);//je copie cette chaine qui est scanner dans ma variable temp
+                //printf("%d\n",strlen(temp));
                 //printf("%s\n",temp);
                     if(stricmp(temp, p.mel_p) == 0){//utilisation strcmp qui permet de comparer deux chaine si les deux chaine sont égales alors la chaine et deja presente donc mel_p n'est pas valide
                         printf("la chaine :%s existe deja\n",p.mel_p);
@@ -305,9 +345,15 @@ MEL:{
 //puts("les caracteres speciaux ne sont pas acceptees -> profession!!!\n");
 PROFESSION:{
             printf("profession\n");
-            fgets(p.profession_p,sizeof(p.profession_p),stdin);
+            fgets(p.profession_p,TAILLE,stdin);
             p.profession_p[strcspn(p.profession_p,"\r\n")] = '\0';
 
+                    if(p.profession_p[0] != '\0'){
+                   p.profession_p = (char*)realloc(p.profession_p,strlen(p.profession_p)*sizeof(char));
+                    }
+                    else{
+                            exit(1);
+                    }
             for(int i = 0;i<strlen(p.profession_p);i++){
             if((p.profession_p[i]>='a' && p.profession_p[i]<='z') || (p.profession_p[i]>='A' && p.profession_p[i]<='Z')|| p.profession_p[i]=='-'){
                     continue;
@@ -321,6 +367,16 @@ PROFESSION:{
         }
 }
 
+
+
+
+
+free(temp);
+free(chaine2);
+free(p.prenom_p);
+free(p.ville_p);
+free(p.mel_p);
+free(p.profession_p);
 
 }
 void Verifier_validite_annuaire(const doublons *d){
@@ -350,6 +406,7 @@ while(!feof(fichier)&& !ferror(fichier)){//si ferror est vrai alors le fichier n
             }
    }
    fclose(fichier);
+
 
 }
 
