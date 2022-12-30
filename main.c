3d9666e00b6ac33df43d2306d9e5069bf1506ea0
BOUCHENNA RAFIK p2200950 <rafik.bouchenna@etu.univ-lyon1.fr>
2022-12-30 18:21:35 +0000

    Commentaire doxygen

--- main.c
+++ main.c
@@ -1,105 +1,72 @@
+/**<
+*\file fonction.c
+*\brief L'objectif de ce projet etait de concevoir une application
+        de gestion d'un annuaire pour une organisation qui possedent un ensemble de données sur ses clients
+        des fonctions pour repondre a cette sont donnes si dessous avec des contraintes spécifiques
+*\author Rafik BOUCHENNA G4S1B
+*\date 08/01/2023
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include<string.h>
 #include "fonction.h"
 
-
-/** \brief
- *
- * \param
- * \param
- * \return
- *
- */
-
-/*void(const afficher  personne)*pp*/
-
-/*
-struct A {
-
-
-char * ad;
-char ch[50];
-
-}
-typedef struct A A;
-void saisie(A*a){
-
-a->ad =   (char *) malloc (20*sizeof(char));
-fgets(a->ad,20,stdin);
-}
-double *p = (double*)malloc(10*sizeof(double));
-char *c = (char*)malloc(15*sizeof(char));
-
-*/
-
-
 int main()
 {
-    Client d;
 
-    //ajouter_Client(&d);
-    //Verifier_validite_annuaire(&d,1);
-
-
-//double *p = (double*)malloc(10*sizeof(double));//80 octets
-//char *c = (char*)malloc(15*sizeof(char));//15 octets
-//calloc : initialise tout a 0 voir -> cppreference
-
-//doublons d;
 int n;
 char c;
+Client d;
 
 
+        MENU:{
+            printf("1-> ajout Client\n");
+            printf("2-> Afficher annuaire Client\n");
+            printf("3-> Verifier la validité de l'annuaire\n");
+            printf("4-> trier Client par le nom\n");
+            printf("5-> Ecriture annuaire\n");
+            printf("6-> filtrer un champs\n");
+        printf("entrer votre choix\n");
+        scanf("%d",&n);
 
-MENU:{
-    printf("1-> ajout Client\n");
-    printf("2-> Afficher annuaire Client\n");
-    printf("3-> Verifier la validité de l'annuaire\n");
-    printf("4-> trier Client par le nom\n");
-    printf("5-> Ecriture annuaire\n");
-    printf("6-> filtrer un champs\n");
-printf("entrer votre choix\n");
-scanf("%d",&n);
+        switch(n){
+        case 1:
+           Ecriture_annuaire_clients(&d);
+            ajouter_Client(&d);
+        break;
+        case 2:
+          Afficher_annuaire_clients(&d);
+        break;
+        case 3:
+            Ecriture_annuaire_clients(&d);
+            Verifier_validite_annuaire(&d,700);
+        break;
+        case 4:
+            Ecriture_annuaire_clients(&d);
+            tri_client_par_nom(&d,1);
+        break;
+        case 5:
+           Ecriture_annuaire_clients(&d);
+          Filtrer_combiner_deux_champs(&d);
+        break;
+        default :
+            printf("erreur");
 
-switch(n){
-case 1:
-   Ecriture_annuaire_clients(&d);
-    ajouter_Client(&d);
-break;
-case 2:
-  Afficher_annuaire_clients(&d);
-break;
-case 3:
-    Ecriture_annuaire_clients(&d);
-    Verifier_validite_annuaire(&d,700);
-break;
-case 4:
-    Ecriture_annuaire_clients(&d);
-    tri_client_par_nom(&d,1);
-break;
-case 5:
-   Ecriture_annuaire_clients(&d);
-  Filtrer_combiner_deux_champs(&d);
-break;
-default :
-    printf("erreur");
+        }
+        printf("\nvoulez vous continuer : entrer o ou O\n");
+        scanf("%c",&c);
 
-}
-printf("\nvoulez vous continuer : entrer o ou O\n");
-scanf("%c",&c);
+        if(c=='o' || c=='O'){
+            goto MENU;
 
-if(c=='o' || c=='O'){
-    goto MENU;
+        }
+        else{
+            exit(1);
+        }
 
-}
-else{
-    exit(1);
-}
-
- return 0;
-}
+         return 0;
+        }
 }
 
 
