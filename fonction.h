31c0137c3f4c400054e4055cc0e990598cf18d5a
BOUCHENNA RAFIK p2200950 <rafik.bouchenna@etu.univ-lyon1.fr>
2022-12-31 20:06:25 +0000

    le fichier .h

--- fonction.h
+++ fonction.h
@@ -1,5 +1,3 @@
-
-
 /**
  *\file fonction.h
  *\brief Si dessous vous trouverais les différentes
@@ -14,7 +12,7 @@
 #pragma once//programmation modulaire
 #define SIZE 20
 
-struct Client_{
+typedef struct Client_{
 
         char *nom_p;
         char *prenom_p;
@@ -23,19 +21,20 @@ struct Client_{
         char telephone_p[SIZE];
         char *mel_p;
         char *profession_p;
+        char **donnes;//tableau 2D allocation dynamique
         char val_chaine1[256];
         char val_chaine2[256];
         char nom_champ1[256];
         char nom_champ2[256];
 
-};
-typedef struct Client_ Client;//notation moins lourde
+} Client;
 
+//declaration des fonctions
 void ajouter_Client(  const Client *Id_client,const char *annuaireCSV);
 void Afficher_annuaire_clients(const Client *Id_client);
 void Ecriture_annuaire_clients(const Client *Id_client);
 void Filtrer_combiner_deux_champs(const Client *Id_client,const char *annuaireCSV);
-int Filtrer(char *s1,char *sch1,char *s2,char *sch2,int index,int index2,int cpt);
+int Filtrer(char *s1,char *sch1,int index,int cpt);
 void tri_client_par_nom( const Client*d,int size,const char *annuaireCSV);
 void Verifier_validite_annuaire(const Client*d,int size);
 void Rendre_annuaire_valide(const Client *Id_client);
