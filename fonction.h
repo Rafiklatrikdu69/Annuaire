8c9be02b1b171c27a00d262038c2a5a15bc7ee04
BOUCHENNA RAFIK p2200950 <rafik.bouchenna@etu.univ-lyon1.fr>
2023-01-07 22:14:14 +0000

    le fichier.h ,ajout d'un structure pour la saisie

--- fonction.h
+++ fonction.h
@@ -11,42 +11,55 @@
 //declaration de la structure client
 #pragma once//programmation modulaire
 #define SIZE 20
+#define TAILLECHAINE 150
 
 typedef struct Client_
 {
 
     char *nom_p;
     char *prenom_p;
-    char code_postal_p[SIZE];
+    char *code_postal_p;
     char *ville_p;
     char telephone_p[SIZE];
     char *mel_p;
     char *profession_p;
     char **donnes;//tableau 2D allocation dynamique
-    char val_chaine1[256];
-    char val_chaine2[256];
-    char nom_champ1[256];
-    char nom_champ2[256];
+    char val_chaine1[TAILLECHAINE];
+    char val_chaine2[TAILLECHAINE];
+    char nom_champ1[TAILLECHAINE];
+    char nom_champ2[TAILLECHAINE];
+    int critere1;
+    int critere2;
+    int choixFiltre ;
+    int index;
+    int index2;
+
 
 } Client;
-
+typedef struct Ajout_Client_ {
+  char nom[TAILLECHAINE];
+  char prenom[TAILLECHAINE];
+  char codepostal[TAILLECHAINE];
+  char ville[TAILLECHAINE];
+  char telephone[TAILLECHAINE];
+  char mail[TAILLECHAINE];
+  char profession[TAILLECHAINE];
+}Ajout_Client;
 //declaration des fonctions
-void saisie_ajout_client(const char *annuaireTXT,const char *nom_p,const char *prenom_p,const char *code_postal_p,const char *ville_p,const char *telephone_p,const char *mel_p,const char *profession_p);
-int ajouter_Client(const char *annuaireTXT,const char *nom_p,const char *prenom_p,const char *code_postal_p,const char *ville_p,const char *telephone_p,const char *mel_p,const char *profession_p);
+void saisie_ajout_client(const char *nom_annuaire,const char *nom_p,const char *prenom_p,const char *code_postal_p,const char *ville_p,const char *telephone_p,const char *mel_p,const char *profession_p);
+void ajouter_Client(const char *nom_annuaire,const char *nom_p,const char *prenom_p,const char *code_postal_p,const char *ville_p,const char *telephone_p,const char *mel_p,const char *profession_p);
 void Afficher_annuaire_clients(const Client *Id_client);
 void Ecriture_annuaire_clients(const Client *Id_client);
-void Filtrer_combiner_deux_champs(const Client *Id_client,const char *annuaireCSV);
-int Filtrer(char *s1,char *sch1,int index,int cpt);
-void tri_client_par_nom( const char *annuaireCSV);
-void Verifier_validite_annuaire(const Client*d,int size);
-void Rendre_annuaire_valide(const Client *Id_client);
-int saisieNOM(const char * nom);
-int saisiePRENOM(const char * prenom);
-int saisieCODEPOSTAL(const char * codepostal);
-int saisieVILLE(const char * ville);
-int saisieTELEPHONE(const char * telephone);
-int saisieMAIL(const char * mail);
-int saisiePROFESSION(const char * profession);
+void Filtrer_combiner_deux_champs(const char *nom_annuaire,const char*nom_champ1,const char*nom_champ2,const char*val_chaine1,const char*val_chaine2);
+int Filtrer(const char *s1,const char *sch1,int cpt);
+void Filtre_avancer(const char *nom_annuaire,const char*nom_champ1,const char*nom_champ2,const char*val_chaine1,const char*val_chaine2,Client*filtre);
+int Filtrer_avancer(const char *s1,const char *sch1,int index,int cpt);
+void tri_client_par_nom( const char *nom_annuaire);
+int Verifier_validite_annuaire(const char * nom_annuaire);
+void Rendre_annuaire_valide(const char *nom_annuaire);
+void SaisieFiltre(Client*filtre);
+void sauvegarde_annuaire(const char*nom_annuaire,struct Client_*p,int size );
+void saisie(Ajout_Client *ajout);
 struct occurence_
 {
 
@@ -56,4 +69,7 @@ struct occurence_
 };
 typedef struct occurence_ occurence;
 
+struct Client_ *tabRetourner(int *size,const char *nom_annuaire);
 
+
+struct Client * tableau(int *taille);
